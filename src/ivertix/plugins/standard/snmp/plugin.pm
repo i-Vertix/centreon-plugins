3a2f3fd7572b83096ae4889b2a120a2b1f3a58f2
rmorandell_pgum <roman.morandell@pgum.eu>
2026-06-24 14:15:43 +0200

    ivertix::plugins::standard::snmp::plugin

--- src/ivertix/plugins/standard/snmp/plugin.pm
+++ src/ivertix/plugins/standard/snmp/plugin.pm
@@ -1,21 +1,5 @@
 #
-# Copyright 2024 Centreon (http://www.centreon.com/)
-#
-# Centreon is a full-fledged industry-strength solution that meets
-# the needs in IT infrastructure and application monitoring for
-# service performance.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Copyright 2026 i-Vertix (http://i-vertix.com/)
 #
 
 package ivertix::plugins::standard::snmp::plugin;
@@ -31,12 +15,28 @@ sub new {
 
     $self->{version} = '1.0';
     $self->{modes} = {
+        'arp'             => 'snmp_standard::mode::arp',
         'cpu'             => 'snmp_standard::mode::cpu',
+        'cpu-detailed'    => 'snmp_standard::mode::cpudetailed',
+        'diskio'          => 'snmp_standard::mode::diskio',
+        'disk-usage'      => 'snmp_standard::mode::diskusage',
+        'entity'          => 'snmp_standard::mode::entity',
+        'inodes'          => 'snmp_standard::mode::inodes',
         'interfaces'      => 'snmp_standard::mode::interfaces',
-        'list-interfaces' => 'snmp_standard::mode::listinterfaces',
         'load'            => 'snmp_standard::mode::loadaverage',
+        'list-diskio'     => 'snmp_standard::mode::listdiskio',
+        'list-diskspath'  => 'snmp_standard::mode::listdiskspath',
+        'list-interfaces' => 'snmp_standard::mode::listinterfaces',
+        'list-processes'  => 'snmp_standard::mode::listprocesses',
+        'list-storages'   => 'snmp_standard::mode::liststorages',
         'memory'          => 'snmp_standard::mode::memory',
-        'uptime'          => 'snmp_standard::mode::uptime'
+        'processcount'    => 'snmp_standard::mode::processcount',
+        'storage'         => 'snmp_standard::mode::storage',
+        'swap'            => 'snmp_standard::mode::swap',
+        'time'            => 'snmp_standard::mode::ntp',
+        'tcpcon'          => 'snmp_standard::mode::tcpcon',
+        'udpcon'          => 'snmp_standard::mode::udpcon',
+        'uptime'          => 'snmp_standard::mode::uptime',
     };
 
     return $self;
