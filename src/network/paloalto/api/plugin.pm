d9936470912c5f065a6bc95170a69f4d7618da7d
rmorandell_pgum <roman.morandell@pgum.eu>
2026-08-04 13:43:41 +0200

    Revert "mode sdwan"
    This reverts commit 79bf496419707d30ca659cd4e4856fe6c81bf747.

--- src/network/paloalto/api/plugin.pm
+++ src/network/paloalto/api/plugin.pm
@@ -38,8 +38,7 @@ sub new {
         'ipsec'       => 'network::paloalto::api::mode::ipsec',
         'discovery'   => 'network::paloalto::api::mode::discovery',
         'health'      => 'network::paloalto::api::mode::health',
-        'certificate' => 'network::paloalto::api::mode::certificate',
-        'sdwan'       => 'network::paloalto::api::mode::sdwan'
+        'certificate' => 'network::paloalto::api::mode::certificate'
     };
 
     $self->{custom_modes}->{api} = 'network::paloalto::api::custom::api';
